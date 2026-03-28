83a228a004e74b3c7c10a5da36d018811a5163a4
successvishal9922 <success.vishal9922@gmail.com>
2026-03-28 21:29:37 +0530

    search element

--- LinkedLIst/SearchElement.java
+++ LinkedLIst/SearchElement.java
@@ -3,14 +3,36 @@ package LinkedLIst;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+class InnerSearchElement {
+
+    
+}
+
 public class SearchElement {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         LinkedList<Integer> L = new LinkedList<>();
         System.out.println("Add elements in List");
 
-        for (int i = 0; i <    ; i++) {
-            
+        for (int i = 0; i < 15   ; i++) {
+            int num = sc.nextInt();
+            L.add(num);
         }
+
+        System.out.println("enter ele you want to search");
+
+        int ele = sc.nextInt();
+
+        
+            if (L.contains(ele)) {
+                System.out.println("Element is present in List");
+            
+            } 
+            
+            else {
+                System.out.println("Element is missing...");
+            }
+            
+        sc.close();
     }
 }
